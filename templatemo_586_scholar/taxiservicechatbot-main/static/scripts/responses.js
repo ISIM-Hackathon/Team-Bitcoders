60cc4bfdbb581d6329af075467c656d89f2ffbac
nehavaishnav1 <nehavaishnav250@gamil.com>
2024-03-09 11:13:42 +0530

    first commit

---
+++ templatemo_586_scholar/taxiservicechatbot-main/static/scripts/responses.js
@@ -0,0 +1,34 @@
+function getBotResponse(input) {
+
+    }
+    if (input == "What services you offer") {
+        return "We currently offer Uber-Moto,Uber Cab and Uver Auto ";
+    } else if (input == "What is the customer service number?") {
+        return "Here is the number :- 99979778xx";
+
+    }
+    if (input == "Are there any tips for effective revision before the exam?") {
+        return "Focus on weak areas, review key concepts, and solve practice questions to reinforce learning. ";
+    } else if (input == "What should I do on the day before the exam?") {
+        return "Review key concepts lightly, get adequate rest, and ensure you have all necessary materials ready.";
+
+    }
+    if (input == "What services you offer") {
+        return "We currently offer Uber-Moto,Uber Cab and Uver Auto ";
+    } else if (input == "What is the customer service number?") {
+        return "Here is the number :- 99979778xx";
+
+    }if (input == "What services you offer") {
+        return "We currently offer Uber-Moto,Uber Cab and Uver Auto ";
+    } else if (input == "What is the customer service number?") {
+        return "Here is the number :- 99979778xx";
+
+    }if (input == "What services you offer") {
+        return "We currently offer Uber-Moto,Uber Cab and Uver Auto ";
+    } else if (input == "What is the customer service number?") {
+        return "Here is the number :- 99979778xx";
+
+    }
+
+    
+   
